205868caf4c040b21cdfe4ade2f64a185f0298df
ksmuch <xislasis@gmail.com>
2025-10-07 14:23:15 +0300

    img fix again

--- src/data.js
+++ src/data.js
@@ -2,79 +2,79 @@ import { RANDOMIZED } from "./settings";
 
 const cats = [{
   'id': 'YdAqiUkUoWA',
-  'url': './assets/cats-1.jpg',
+  'url': './assets/img/cats-1.jpg',
   'description': '',
 }, {
   'id': 'hX_hf2lPpUU',
-  'url': './assets/cats-2.jpg',
+  'url': './assets/img/cats-2.jpg',
   'description': '',
 }, {
   'id': 'w1JE5duY62M',
-  'url': './assets/cats-3.jpg',
+  'url': './assets/img/cats-3.jpg',
   'description': '',
 }, {
   'id': '3tYZjGSBwbk',
-  'url': './assets/cats-4.jpg',
+  'url': './assets/img/cats-4.jpg',
   'description': '',
 }, {
   'id': 'NoXUQ54pDac',
-  'url': './assets/cats-5.jpg',
+  'url': './assets/img/cats-5.jpg',
   'description': '',
 }, {
   'id': 'OZhYgZh0bAg',
-  'url': './assets/cats-6.jpg',
+  'url': './assets/img/cats-6.jpg',
   'description': '',
 }];
 
 const cars = [{
   'id': 'YdAqiUkUoWA',
-  'url': './assets/cars-1.jpg',
+  'url': './assets/img/cars-1.jpg',
   'description': '',
 }, {
   'id': 'hX_hf2lPpUU',
-  'url': './assets/cars-2.jpg',
+  'url': './assets/img/cars-2.jpg',
   'description': '',
 }, {
   'id': 'w1JE5duY62M',
-  'url': './assets/cars-3.jpg',
+  'url': './assets/img/cars-3.jpg',
   'description': '',
 }, {
   'id': '3tYZjGSBwbk',
-  'url': './assets/cars-4.jpg',
+  'url': './assets/img/cars-4.jpg',
   'description': '',
 }, {
   'id': 'NoXUQ54pDac',
-  'url': './assets/cars-5.jpg',
+  'url': './assets/img/cars-5.jpg',
   'description': '',
 }, {
   'id': 'OZhYgZh0bAg',
-  'url': './assets/cars-6.jpg',
+  'url': './assets/img/cars-6.jpg',
   'description': '',
 }];
 
 const flowers = [{
   'id': 'YdAqiUkUoWA',
-  'url': './assets/flowers-1.jpg',
+  'url': './assets/img/flowers-1.jpg',
   'description': '',
 }, {
   'id': 'hX_hf2lPpUU',
-  'url': './assets/flowers-2.jpg',
+  'url': './assets/img/flowers-2.jpg',
   'description': '',
 }, {
   'id': 'w1JE5duY62M',
-  'url': './assets/flowers-3.jpg',
+  'url': './assets/img/flowers-3.jpg',
   'description': '',
 }, {
   'id': '3tYZjGSBwbk',
-  'url': './assets/flowers-4.jpg',
+  'url': './assets/img/flowers-4.jpg',
   'description': '',
 }, {
   'id': 'NoXUQ54pDac',
-  'url': './assets/flowers-5.jpg',
+  'url': './assets/img/flowers-5.jpg',
   'description': '',
 }, {
   'id': 'OZhYgZh0bAg',
-  'url': './assets/flowers-6.jpg',
+  'url': './assets/img/flowers-6.jpg',
   'description': '',
 }];
 
